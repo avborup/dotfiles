1aa9ae89b1714768387a2cc152e6aa9681f7cd33
Adrian Borup <avborup@gmail.com>
2021-10-04 18:38:16 +0200

    powershell: use powershell 7 profile location

--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -1,4 +1,10 @@
-Push-Location $HOME\Documents\WindowsPowerShell
+# Assumes PowerShell 7
+$profile_dir = "$HOME\Documents\PowerShell"
+if (-Not (Test-Path ($profile_dir))) {
+    New-Item -ItemType "directory" -Path $profile_dir | Out-Null
+}
+
+Push-Location $profile_dir
 try {
     New-Item -ItemType SymbolicLink -Name Microsoft.PowerShell_profile.ps1 -Target $HOME/dotfiles/powershell/Microsoft.PowerShell_profile.ps1 -ea Stop
     echo 'Linked Microsoft.PowerShell_profile.ps1'
